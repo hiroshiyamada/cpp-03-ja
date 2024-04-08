7b8ad9c42396462f647322be5800ae148ecb682e
Hiroshi Yamada <hiroshi.yamada426@gmail.com>
2024-04-08 12:03:01 +0900

    Add m4-u1-c3 codes

--- 01_various/03_challenge/main.cpp
+++ 01_various/03_challenge/main.cpp
@@ -1,1 +1,12 @@
-// コードを入力してください
+#include "templates.hpp"
+#include <iostream>
+
+int main()
+{
+    // 出力: 1 (true)
+    std::cout << allTrue(true, true, true) << std::endl;
+    // 出力: 0 (false)
+    std::cout << allTrue(true, false, true) << std::endl;
+    // 出力: 1 (true)
+    std::cout << allTrue() << std::endl;
+}
--- 01_various/03_challenge/templates.hpp
+++ 01_various/03_challenge/templates.hpp
@@ -1,1 +1,10 @@
-// コードを入力してください
+bool allTrue()
+{
+    return true;
+}
+
+template <typename... Args>
+bool allTrue(bool head, Args... args)
+{
+    return head && allTrue(args...);
+}
