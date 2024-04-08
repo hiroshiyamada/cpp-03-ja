17ead6c511d99f3c585049f19e5f8609d6fa809a
Hiroshi Yamada <hiroshi.yamada426@gmail.com>
2024-04-08 11:42:47 +0900

    Add m4-u1-c1 codes

--- 01_various/01_challenge/timer.cpp
+++ 01_various/01_challenge/timer.cpp
@@ -1,1 +1,12 @@
-// コードを入力してください
+#include "timer.hpp"
+#include <iostream>
+
+Timer::Timer(){
+    start_time = std::chrono::high_resolution_clock::now();
+}
+
+Timer::~Timer(){
+    std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+    std::cout << duration.count() << " ms has passed." << std::endl;
+}
